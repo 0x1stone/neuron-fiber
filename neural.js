38a32d9d93f746e6d2571eb7220876082c2b9264
liushitao <liushitao@bilibili.com>
2017-08-08 16:51:37 +0800

    up

--- neural.js
+++ neural.js
@@ -1,46 +1,72 @@
 const numeric = require('numeric')
-function nonlin (x, deriv) {
-  if (deriv) {
-    return numeric.mul(x, numeric.sub(1, x));
+
+class Perceptron {
+
+  constructor(x, y, iteration) {
+    // rate 学习率
+    // iteration 训练次数
+    // w 权重向量
+    // errors 出错次数
+    this.x = x
+    this.y = y
+    this.resultY
+    this.w
+    this.iteration = iteration
+
+    this.initWeight()
+    this.train()
   }
-  return numeric.div(1, numeric.add(1, numeric.exp(numeric.neg(x)))); 
+
+  initWeight () {
+    this.w = numeric.sub(numeric.mul(2, numeric.random([3, 1])), 1)
+  }
+
+  predict (x) {
+
+  }
+
+  sigmoid (x) {
+    return numeric.div(1, numeric.add(1, numeric.exp(numeric.neg(x))))
+  }
+
+  derivSigmoid (x) {
+    return numeric.mul(x, numeric.sub(1, x))
+  }
+
+  train () {
+    for (let i = 1; i <= this.iteration; i++) {
+      // 权重更新算法
+      // W(j)=W(j)+delta W(j)
+      // delata W(j)= 学习率n * (Y-Y')* .X(j)
+
+      // 学习率 derivSigmoid() 来构建,代表sigmoid的倒数，即斜率
+
+      this.resultY = this.sigmoid(numeric.dot(this.x, this.w))  // Y'
+      const deltaY = numeric.sub(y, this.resultY)  // (Y-Y')
+      const deltaW = numeric.dot(
+        numeric.transpose(this.x),
+        numeric.mul(this.derivSigmoid(this.resultY), deltaY)
+      ) // n * (Y-Y') .* X(j)
+
+      this.w = numeric.add(this.w, deltaW) // W(j)=W(j)+delta W(j)
+    }
+    console.log(this.w)
+    console.log(this.resultY)
+  }
 }
 
-function train_neural (X, y, iteration) {
-  // initialize weights
-  var syn0 = numeric.sub(numeric.mul(2, numeric.random([3, 1])), 1); // 权重W 初始化 [0~1]之间
-  //Training loop
-  var i = 0;
-  var l1
-  for (; i < iteration; i++) {
-    // 权重更新算法
-    // W(j)=W(j)+delta W(j)
-    // delata W(j)= 学习率n * (Y-Y')* X(j)
-
-    // 学习率 [0,1]之间的一个小数
-    var l0 = X;
-    var l1 = nonlin(numeric.dot(l0, syn0)); 
-    var l1_error = numeric.sub(y, l1); // 偏移量 loss
-    var l1_delta = numeric.mul(l1_error, nonlin(l1, true));
-    
-    syn0 = numeric.add(syn0, numeric.dot(numeric.transpose(l0), l1_delta)); // 相关性矩阵
-  }
-  return { syn0, l1 }
-}
-
-//Initial input/ouput values
-var X = [
+var x = [
   [0, 0, 1],
   [0, 1, 1],
   [1, 0, 1],
   [1, 1, 1]
-];
+]
 
 var y = [
   [0],
   [0],
   [1],
   [1]
-];
+]
 
-console.log(train_neural(X, y, 1))
+new Perceptron(x, y, 1000)
---
+++ readme.md
@@ -0,0 +1,16 @@
+# 基于js的神经网络实现
+
+> By stone
+
+
+## 概念说明
+
+权重更新算法
+W(j)=W(j)+delta W(j)
+delata W(j)= 学习率n * (Y-Y')* .X(j)
+
+学习率 由derivSigmoid() 来构建,代表sigmoid的倒数，即斜率
+
+
+
+
