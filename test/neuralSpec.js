64170796c2bd0e84840f32d5cd7c2139e3a8330c
liushitao <liushitao@bilibili.com>
2018-05-02 11:50:29 +0800

    test case

--- test/neuralSpec.js
+++ test/neuralSpec.js
@@ -1,12 +1,22 @@
 describe('neural', function() {
   var x = [[0, 0, 1, 0], [0, 1, 1, 0], [1, 0, 1, 0], [1, 1, 1, 0]]
   var y = [[0], [0], [1], [1]]
-  var Neural = require('../src/neuron-core.js')
-  var neural
+  var testdata = [[0, 0, 1, 0]]
+
+  var { NeuronNet, NeuronLayer } = require('../dist/index')
+
+  var neuralNet, result
   beforeEach(function() {
-    neural = new Neural(x, y, 1000)
+    neuronNet = new NeuronNet(x, y, 1000)
+    neuronNet
+      .link(new NeuronLayer(5))
+      .link(new NeuronLayer(3))
+      .link(new NeuronLayer(1))
+      
+    neuronNet.train()
+    result = neuronNet.predict(testdata)
   })
   it('shoulbe be a object', function() {
-    expect(neural).not.toEqual(null)
+    expect(neuronNet.predict(testdata)).not.toEqual(null)
   })
 })
